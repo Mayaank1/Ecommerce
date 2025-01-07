7e7df415c4b088c5aec0a28e9876502473d72e96
Mayaank1 <mayaankaekka115@gmail.com>
2025-01-07 22:56:49 +0530

    Shopping-cart done

--- 03-Frontend/my-app/src/app/components/product-details/product-details.component.ts
+++ 03-Frontend/my-app/src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../common/product';
 import { ProductService } from '../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { CardItem } from '../../common/card-item';
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +14,8 @@ export class ProductDetailsComponent implements OnInit {
   product!: Product;
   constructor(
     private productService: ProductService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cartService: CartService
   ) {}
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
@@ -25,4 +28,8 @@ export class ProductDetailsComponent implements OnInit {
       this.product = data;
     });
   }
+  addToCart() {
+    const theCartItem = new CardItem(this.product);
+    this.cartService.addToCart(theCartItem);
+}
 }
