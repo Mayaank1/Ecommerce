aba1ef643d4c19b5daf7e8b0c903c55246665164
Mayaank1 <mayaankaekka115@gmail.com>
2025-01-11 22:34:42 +0530

    final commit

--- 03-Frontend/my-app/src/app/components/checkout/checkout.component.ts
+++ 03-Frontend/my-app/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-checkout',
@@ -10,7 +11,9 @@ export class CheckoutComponent implements OnInit {
   checkoutFormGroup: FormGroup;
   totalPrice: number = 0;
   totalQuantity: number = 0;
-  constructor(private formBuilder:FormBuilder) { }
+
+  
+  constructor(private formBuilder:FormBuilder,private cartService : CartService) { }
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -42,6 +45,7 @@ export class CheckoutComponent implements OnInit {
         expirationYear: ['']
       })
     });
+    this.totalQuantityAndPrice();
   }
 onSubmit(){
   console.log(this.checkoutFormGroup.value);
@@ -54,5 +58,14 @@ copyShippingAddressToBillingAddress(event){
   else{
     this.checkoutFormGroup.controls['billingAddress'].reset();
   }
+}
+
+totalQuantityAndPrice(){
+  this.cartService.totalPrice.subscribe(
+    data => this.totalPrice = data
+  );
+  this.cartService.totalQuantity.subscribe(
+    data => this.totalQuantity = data
+  );
 }
 }
