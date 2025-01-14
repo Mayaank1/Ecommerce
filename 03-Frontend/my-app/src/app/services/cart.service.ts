165f02415bf305d925af5a57477028a28c22047f
Mayaank1 <mayaankaekka115@gmail.com>
2025-01-14 22:19:33 +0530

    BehaviourSubject

--- 03-Frontend/my-app/src/app/services/cart.service.ts
+++ 03-Frontend/my-app/src/app/services/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CardItem } from '../common/card-item';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +10,8 @@ export class CartService {
 
   cartItems: CardItem[] = [];
   
-  totalPrice: Subject<number> = new Subject<number>();
-  totalQuantity: Subject<number> = new Subject<number>();
+  totalPrice: Subject<number> = new BehaviorSubject<number>(0);
+  totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
   constructor() { }
 
   addToCart(theCartItem: CardItem){
