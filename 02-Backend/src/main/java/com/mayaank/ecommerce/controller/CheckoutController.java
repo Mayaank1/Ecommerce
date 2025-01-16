9432ce3fb1d23e0a1cda36a0eab4a7f6db8d7e21
Mayaank1 <mayaankaekka115@gmail.com>
2025-01-16 22:05:41 +0530

    Controller

--- 02-Backend/src/main/java/com/mayaank/ecommerce/controller/CheckoutController.java
+++ 02-Backend/src/main/java/com/mayaank/ecommerce/controller/CheckoutController.java
@@ -1,12 +1,33 @@
 package com.mayaank.ecommerce.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mayaank.ecommerce.dto.Purchase;
+import com.mayaank.ecommerce.dto.PurchaseResponse;
+import com.mayaank.ecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+
+
+
+    }
+
+
+
 
 }
