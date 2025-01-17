f62feabb78433346e110bac9a5e481b2eb348c85
Mayaank1 <mayaankaekka115@gmail.com>
2025-01-17 18:51:40 +0530

    config

--- 03-Frontend/my-app/src/app/config/my-app-config.ts
+++ 03-Frontend/my-app/src/app/config/my-app-config.ts
@@ -1,2 +1,11 @@
-export interface MyAppConfig {
+export default {
+
+    oidc: {
+        clientId: '0oampm9jpaX9xgHDO5d7',
+        issuer: 'https://dev-11134952.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+
+    }
+
 }
