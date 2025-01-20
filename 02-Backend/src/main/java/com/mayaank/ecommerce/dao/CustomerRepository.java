7432af652e90a8cdf1a94f7fcd6dedd8c507af39
Mayaank1 <mayaankaekka115@gmail.com>
2025-01-20 16:44:12 +0530

    email

--- 02-Backend/src/main/java/com/mayaank/ecommerce/dao/CustomerRepository.java
+++ 02-Backend/src/main/java/com/mayaank/ecommerce/dao/CustomerRepository.java
@@ -4,6 +4,6 @@ import com.mayaank.ecommerce.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
-   // Customer findByEmail(String theEmail);
+    Customer findByEmail(String theEmail);
 
 }
